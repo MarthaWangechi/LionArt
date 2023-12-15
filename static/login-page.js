39e3e67ad19cd2c1cb5ea63799a54106226ed084
Shelby Wengreen <shelby.wengreen@columbia.edu>
2023-12-15 18:29:35 -0500

    Add login/create user functionality

--- static/login-page.js
+++ static/login-page.js
@@ -1,4 +1,34 @@
-function redirectToLandingPage() {
-    console.log("test")
-    window.location.href = "{{ url_for('display_landing_page') }}"
+function login(username, password) {
+    var userData = {
+        "username": username,
+        "password": password
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "Login", 
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(userData),
+        success: function(result) {
+            console.log("Success! User Logged in!", userData);
+            $('#error-message').text("");
+            window.location.href = '/LandingPage'; 
+        },
+        error: function(xhr, status, error) {
+            console.log("Error");
+            console.log(xhr.responseJSON.message);
+            $('#error-message').text(xhr.responseJSON.message);
+        }
+    });
 }
+
+$(document).ready(function(){
+
+    $("#loginButton").click(function(){
+        console.log("SHELBY LOGIN BUTTON CLICKED")
+        var username = $("#username").val()
+        var password = $("#password").val()
+        login(username, password);
+    })
+})
